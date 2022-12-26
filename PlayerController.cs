2519a2960922e49b451d7fb76155da83c5ca16f8
doron <doronn89@gmail.com>
2022-12-26 09:26:58 +0200

    FirstCommit: Added player controller and properties scripts

---
+++ PlayerController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UnityEngine;
+
+namespace PlayerController
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public PlayerProperties playerProperties; // The scriptable object with the gameplay properties
+        public float _movementInterpolationSpeed;
+
+        private Transform _playerTransform;      // Reference to the player's transform component
+        private bool _isGrounded = false;        // Flag to track whether the player is grounded
+        private int _jumpsRemaining = 0;         // The number of jumps remaining
+        private Vector3 _velocity = Vector3.zero; // The current velocity of the 
+        private Vector3 _currentVelocity = Vector3.zero;
+        private Vector3 _nextWantedPosition = Vector3.zero;
+
+        private bool _didRequestJump = false;
+
+        private void Start()
+        {
+            _playerTransform = GetComponent<Transform>();
+            _nextWantedPosition = _playerTransform.localPosition;
+        }
+
+        private void FixedUpdate()
+        {
+            // Get input for horizontal movement
+            float horizontalInput = Input.GetAxis("Horizontal");
+            Vector3 moveDirection = new Vector3(horizontalInput, 0f, 0f);
+
+            if (_isGrounded || Math.Abs(horizontalInput) > 0.5f || _velocity.y < 0)
+            {
+                _velocity.x = horizontalInput * playerProperties.moveSpeed;
+            }
+            // _nextWantedPosition += moveDirection * (playerProperties.moveSpeed * Time.deltaTime);
+
+            // Get input for jumping
+            if (_didRequestJump && (_isGrounded || _jumpsRemaining > 0))
+            {
+                _velocity.y = playerProperties.jumpForce;
+                _jumpsRemaining--;
+            }
+
+            _didRequestJump = false;
+            
+            if (!_isGrounded)
+            {
+                // Apply gravity to the player
+                _velocity += Vector3.down * playerProperties.gravity;
+            }
+            
+            _currentVelocity = _velocity * Time.deltaTime;
+            _nextWantedPosition += _currentVelocity;
+            PlayerCollisionCheck(Vector3.up, playerProperties.solidGroundLayer);
+            PlayerCollisionCheck(Vector3.left, playerProperties.solidGroundLayer);
+            PlayerCollisionCheck(Vector3.right, playerProperties.solidGroundLayer);
+            PlayerCollisionCheck(Vector3.down, playerProperties.groundLayer);
+        }
+
+        private void Update()
+        {
+            _didRequestJump |= Input.GetButtonDown("Jump");
+            var currentTransformPosition = _playerTransform.localPosition;
+            currentTransformPosition = Vector3.Lerp(currentTransformPosition, _nextWantedPosition, _movementInterpolationSpeed * Time.deltaTime);
+            _playerTransform.localPosition = currentTransformPosition;
+        }
+
+        private void PlayerCollisionCheck(Vector3 direction, int collisionLayerMask)
+        {
+            // Check if the player is grounded by casting a ray down from the player's position
+            var velocityInDirection = Vector3.Dot(direction, _velocity);
+            var velocityFactorToAdd = velocityInDirection < 0 ? velocityInDirection * Time.deltaTime : 0;
+
+            var groundCheckDistance = Math.Abs(Vector3.Dot(direction, playerProperties.CharacterSize) * 0.5f);
+            var rayDistance = groundCheckDistance + velocityFactorToAdd;
+            var rayEndPosition = _nextWantedPosition + direction * rayDistance;
+            if (Physics.Raycast(_nextWantedPosition, direction, out var hit, rayDistance, collisionLayerMask))
+            {
+                // Set the player as grounded if it is falling onto the platform from above
+                Debug.DrawLine(_nextWantedPosition, hit.point, Color.blue);
+                Debug.DrawLine(hit.point, rayEndPosition, Color.red);
+                var hitHorizontalForHorizontalDirectionCheck = hit.normal.x > 0 != direction.x > 0;
+                var hitVerticalForVerticalDirectionCheck = hit.normal.y > 0 != direction.y > 0;
+                if (hitHorizontalForHorizontalDirectionCheck || hitVerticalForVerticalDirectionCheck)
+                {
+                    _isGrounded = direction.y < 0;
+                    if (_isGrounded)
+                    {
+                        _jumpsRemaining = playerProperties.maxJumps;
+                    }
+
+                    // Set the player's Y position to the Y position of the ground collider
+                    var position = _nextWantedPosition;
+                    var nextXPosition = hitHorizontalForHorizontalDirectionCheck ? hit.collider.ClosestPointOnBounds(hit.point).x + -direction.x * groundCheckDistance : position.x;
+                    var nextYPosition = hitVerticalForVerticalDirectionCheck ? hit.collider.ClosestPointOnBounds(hit.point).y + -direction.y * groundCheckDistance : position.y;
+                    position =
+                        new Vector3(nextXPosition, nextYPosition, position.z);
+                    _nextWantedPosition = position;
+
+                    if (hitVerticalForVerticalDirectionCheck)
+                    {
+                        _velocity.y = 0f;
+                    }
+                }
+                else
+                {
+                    _isGrounded = false;
+                }
+            }
+            else
+            {
+                Debug.DrawLine(_nextWantedPosition, rayEndPosition, Color.blue);
+                _isGrounded = false;
+            }
+        }
+    }
+}
---
+++ PlayerProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PlayerController
+{
+    [CreateAssetMenu(menuName = "Player Properties")]
+    public class PlayerProperties : ScriptableObject
+    {
+        public float moveSpeed = 10f;            // The speed at which the player moves
+        public float jumpForce = 10f;            // The force of the player's jump
+        public float gravity = 9.81f;            // The gravitational force applied to the player
+        public float groundCheckDistance = 0.5f; // The distance at which the player is considered to be grounded
+        public LayerMask groundLayer;            // The layer mask for the ground colliders
+        public LayerMask solidGroundLayer;       // The layer mask for the non passable ground colliders (that cannot be jumped through)
+        public int maxJumps = 1;                 // The maximum number of jumps allowed
+        public Vector3 CharacterSize = Vector3.one;       // The size of the character
+    }
+}
